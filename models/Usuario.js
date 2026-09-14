28a2816c21a60f7a0d1ffaeee85e40f6a7881795
mamiranda20 <123marcela@gmail.com>
2026-09-14 23:19:36 +0000

    Criação de modelo de usuário

---
+++ models/Usuario.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose'); 
+
+ 
+
+const usuarioSchema = new mongoose.Schema({ 
+
+  nome: { type: String, required: true }, 
+
+  email: { type: String, required: true, unique: true }, 
+
+  senhaHash: { type: String, required: true } 
+
+}); 
