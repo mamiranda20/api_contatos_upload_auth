2778fc60b434011b3f22b2a4b8a631531c4c4d52
mamiranda20 <123marcela@gmail.com>
2026-09-14 23:24:05 +0000

    Modelo contato

---
+++ models/Contato.js
@@ -0,0 +1,19 @@
+const contatoSchema = new mongoose.Schema({ 
+
+  nome: { type: String, required: true }, 
+
+  email: String, 
+
+  telefone: String, 
+
+  endereco: String, 
+
+  fotoId: { type: mongoose.Schema.Types.ObjectId }, // referência ao arquivo no GridFS 
+
+  usuarioId: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true } 
+
+}); 
+
+ 
+
+module.exports = mongoose.model('Contato', contatoSchema); 
